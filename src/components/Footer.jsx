f9d236b464587f11a7a606a71116828b7b06d8f8
srikanth <srikanth@gmail.com>
2024-08-06 14:16:17 +0530

    add the file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,22 @@
-// import React from 'react'
+import React from 'react'
 
-// function Footer() {
-//   return (
-//     <div>
-//     <footer className="bg-dark text-white py-4">
-//       <div className="container text-center">
-//         <p>&copy; 2024 Your Company. All rights reserved.</p>
-//         <ul className="list-inline">
-//           <li className="list-inline-item"><a href="/" className="text-white">Privacy Policy</a></li>
-//           <li className="list-inline-item"><a href="/" className="text-white">Terms of Service</a></li>
-//           <li className="list-inline-item"><a href="/" className="text-white">Contact Us</a></li>
-//         </ul>
-//       </div>
-//     </footer>
+function Footer() {
+  return (
+    <div>
+    <footer className="bg-dark text-white py-4">
+      <div className="container text-center">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <ul className="list-inline">
+          <li className="list-inline-item"><a href="/" className="text-white">Privacy Policy</a></li>
+          <li className="list-inline-item"><a href="/" className="text-white">Terms of Service</a></li>
+          <li className="list-inline-item"><a href="/" className="text-white">Contact Us</a></li>
+        </ul>
+      </div>
+    </footer>
+    </div>
+  )
+}
  
 
-//     </div>
-//   )
-// }
 
-// export default Footer
+ export default Footer
